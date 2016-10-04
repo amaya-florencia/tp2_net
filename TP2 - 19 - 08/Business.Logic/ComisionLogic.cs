887f36b7baa4823a9fdcd85a8322ef8dac65c261
ornella <ornella_zorzi@yahoo.com>
2016-10-04 09:34:45 -0300

    comisiones

---
+++ TP2 - 19 - 08/Business.Logic/ComisionLogic.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Data.Database;
+using Business.Entities;
+
+
+namespace Business.Logic
+{
+   public  class ComisionLogic:BusinessLogic
+    {
+        ComisionAdapter _ComisionData;
+
+        public ComisionAdapter ComisionData
+        {
+            get { return _ComisionData; }
+            set { _ComisionData = value; }
+        }
+        ComisionAdapter ca = new ComisionAdapter();
+
+     
+      #region Constructores
+        public ComisionLogic()
+        {
+            ComisionAdapter ComisionData = new ComisionAdapter();
+        }
+        #endregion
+
+        public List<Comision> GetAll()
+        {
+            try
+            {
+                ComisionAdapter cm = new ComisionAdapter();
+                return cm.GetAll();
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error GetAll", Ex);
+                throw ExcepcionManejada;
+            }
+        }
+        public Comision GetOne(int ID)
+        {
+
+            return ca.GetOne(ID);
+        }
+        public void Delete(int ID)
+        {
+
+            ca.Delete(ID);
+        }
+
+        public void Save(Comision com)
+        {
+
+           ca.Save(com);
+        }
+
+
+
+
+    }
+}
